7086acf887ec1b7cb0ecb6f6056164a1ba2a353e
mark-antal-csizmadia <mark.antal.csizmadia@gmail.com>
2023-07-18 18:36:23 +0200

    move tests up to root dir. add `settings.py`. add more tests for pg controller.

--- test/data/test_pg_controller.py
+++ test/data/test_pg_controller.py
@@ -1,13 +1,8 @@
-import os
-from dotenv import load_dotenv, find_dotenv
 import pytest
+from pathlib import Path
 
 from tsboi.data.pg_controller import PostgresController
-
-load_dotenv(find_dotenv())
-PG_DATABASE = os.environ.get("PG_DATABASE")
-PG_USER = os.environ.get("PG_USER")
-PG_PASSWORD = os.environ.get("PG_PASSWORD")
+from settings import PG_DATABASE, PG_USER, PG_PASSWORD
 
 
 @pytest.fixture
@@ -24,15 +19,15 @@ def pg_controller():
 def test_data():
     return [
         ('2021-01-01 00:00:00+00:00', 1.0, 2.0, 0.5, 1.5, 100.0),
-        ('2021-01-02 00:00:00+00:00', 1.5, 2.5, 1.0, 2.0, 200.0),
-        ('2021-01-03 00:00:00+00:00', 2.0, 3.0, 1.5, 2.5, 300.0),
-        ('2021-01-04 00:00:00+00:00', 2.5, 3.5, 2.0, 3.0, 400.0),
-        ('2021-01-05 00:00:00+00:00', 3.0, 4.0, 2.5, 3.5, 500.0),
-        ('2021-01-06 00:00:00+00:00', 3.5, 4.5, 3.0, 4.0, 600.0),
-        ('2021-01-07 00:00:00+00:00', 4.0, 5.0, 3.5, 4.5, 700.0),
-        ('2021-01-08 00:00:00+00:00', 4.5, 5.5, 4.0, 5.0, 800.0),
-        ('2021-01-09 00:00:00+00:00', 5.0, 6.0, 4.5, 5.5, 900.0),
-        ('2021-01-10 00:00:00+00:00', 5.5, 6.5, 5.0, 6.0, 1000.0)]
+        ('2021-01-01 00:01:00+00:00', 1.5, 2.5, 1.0, 2.0, 200.0),
+        ('2021-01-01 00:02:00+00:00', 2.0, 3.0, 1.5, 2.5, 300.0),
+        ('2021-01-02 00:01:00+00:00', 2.5, 3.5, 2.0, 3.0, 400.0),
+        ('2021-01-02 00:02:00+00:00', 3.0, 4.0, 2.5, 3.5, 500.0),
+        ('2021-01-02 00:03:00+00:00', 3.5, 4.5, 3.0, 4.0, 600.0),
+        ('2021-01-03 00:01:00+00:00', 4.0, 5.0, 3.5, 4.5, 700.0),
+        ('2021-01-03 00:02:00+00:00', 4.5, 5.5, 4.0, 5.0, 800.0),
+        ('2021-01-03 00:03:00+00:00', 5.0, 6.0, 4.5, 5.5, 900.0),
+        ('2021-01-03 00:04:00+00:00', 5.5, 6.5, 5.0, 6.0, 1000.0)]
 
 
 def test_open_connection(pg_controller):
@@ -113,6 +108,32 @@ def test_insert_ohlcv_records(pg_controller, test_data):
         assert pg_controller.cursor.fetchone()[0]
 
     # clean up
+    pg_controller.cursor.execute("DROP TABLE test_table")
+    pg_controller.close_connection()
+
+
+def test_get_ohlcv_records(pg_controller, test_data):
+    pg_controller.create_ohlcv_table(table_name="test_table")
+    pg_controller.insert_ohlcv_records(
+        table_name="test_table",
+        tss=[test_data[idx][0] for idx in range(len(test_data))],
+        open_values=[test_data[idx][1] for idx in range(len(test_data))],
+        high_values=[test_data[idx][2] for idx in range(len(test_data))],
+        low_values=[test_data[idx][3] for idx in range(len(test_data))],
+        close_values=[test_data[idx][4] for idx in range(len(test_data))],
+        volume_values=[test_data[idx][5] for idx in range(len(test_data))])
+
+    paths = pg_controller.get_ohlcv_records(
+        table_name="test_table",
+        ts_start=test_data[0][0],
+        ts_end=test_data[-1][0],
+        data_dir=Path("test_data"),
+        granularity="minute",
+        chunk_size=5)
+
+    assert len(paths) == int(len(test_data) // 5)
+    # clean up
+    pg_controller.open_connection()
     pg_controller.cursor.execute("DROP TABLE test_table")
     pg_controller.close_connection()
 
