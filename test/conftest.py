a1ed6f5c8c171cf8ebf7ab7244433b654faff1e0
mark-antal-csizmadia <mark.antal.csizmadia@gmail.com>
2023-07-19 23:55:27 +0200

    improved tests

---
+++ test/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+
+from tsboi.data.pg_controller import PostgresController
+from tsboi.data.cctx_fetcher import CCTXFetcher
+from settings import PG_DATABASE, PG_USER, PG_PASSWORD
+
+
+def pytest_addoption(parser):
+    parser.addoption('--postgres_host', action='store', default='localhost',
+                     help='Postgres host (need to be container name in docker-compose)')
+
+
+@pytest.fixture
+def pg_controller(request):
+    return PostgresController(
+        database=PG_DATABASE,
+        user=PG_USER,
+        password=PG_PASSWORD,
+        host=request.config.getoption('--postgres_host'),
+        port='5432')
+
+
+@pytest.fixture
+def fetcher():
+    return CCTXFetcher("binance")
